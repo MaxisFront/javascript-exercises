b4c290a12b2dd09e58d023321eef843cba47abc0
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-03 13:19:38 -0600

    feat(repeatString): complete all tests
    Return all the 8 expected values from the "spec.js" file. Sixth task
    from "The Odin Project".

--- foundations/solution_06_repeatString/repeatString.js
+++ foundations/solution_06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+  
+  if (times < 0) {
+    return "ERROR";
+  }
 
+  let message = [];
+  while(times > 0) {
+    message.push(string);
+    times--;
+  }
+
+  return message.join("");
 };
 
 // Do not edit below this line
