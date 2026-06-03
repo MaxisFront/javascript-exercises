fde90d9013d369c28c1aeacf867806eb85a80647
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-03 15:03:50 -0600

    feat(sumAll): complete all tests.
    - Fibonacci sequence.
    - Return all 8 expected values from the "spec.js" file. Nintth task from "The Odin Project".

--- foundations/solution_09_sumAll/sumAll.js
+++ foundations/solution_09_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+
+  if (!Number.isInteger(value1) || !Number.isInteger(value2)) return "ERROR";
+  if (value1 < 0 || value2 < 0) return "ERROR";
+  
+  const max = Math.max(value1, value2);
+  const min = Math.min(value1, value2);
+  
+  let sum = 0;
+  let counter = min;
+
+  for (let i = min; i <= max; i++) {
+    sum += counter;
+    counter++;
+  }
+
+  return sum;
 
 };
 
