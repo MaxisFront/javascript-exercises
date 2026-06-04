86b604bb6e31609f7894c56e75dc14d3acfdab3c
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-03 20:23:44 -0600

    feat(tempConversion): complete all tests.
    - From Celcius to Farenheit.
    - From Farenheit to Celcius.
    - Return all 6 expected values from the "spec.js" file. Eleventh task
      from "The Odin Project;"

--- foundations/solution_11_tempConversion/tempConversion.js
+++ foundations/solution_11_tempConversion/tempConversion.js
@@ -1,8 +1,10 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = farenhetis => Math.round(((farenhetis - 32) * (5/9)) * 10) / 10;
+Math.round()
+const convertToFahrenheit = celcius => Math.round(((celcius * 9/5) + (32)) * 10) / 10;
 
-const convertToFahrenheit = function() {
-};
+// °F = (°C * 9/5) + 32
+// °C = (°F - 32) * 5/9
+Math.round()
 
 // Do not edit below this line
 module.exports = {
