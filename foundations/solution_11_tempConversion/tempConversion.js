c2448e4d2dea691fcde2100bf446ac427d5fe1f8
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-03 20:28:19 -0600

    chore(tempConversion): clean comments and function

--- foundations/solution_11_tempConversion/tempConversion.js
+++ foundations/solution_11_tempConversion/tempConversion.js
@@ -1,10 +1,6 @@
 const convertToCelsius = farenhetis => Math.round(((farenhetis - 32) * (5/9)) * 10) / 10;
-Math.round()
-const convertToFahrenheit = celcius => Math.round(((celcius * 9/5) + (32)) * 10) / 10;
 
-// °F = (°C * 9/5) + 32
-// °C = (°F - 32) * 5/9
-Math.round()
+const convertToFahrenheit = celcius => Math.round(((celcius * 9/5) + (32)) * 10) / 10;
 
 // Do not edit below this line
 module.exports = {
