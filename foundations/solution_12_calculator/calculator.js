9990bffbe639945048385dff030ecca50f760a3e
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-25 16:12:28 -0600

    feat(Calculator): complete all tests
    Implement basic arithmetic and math functions to simulate a basic
    calculator.
    
    - Functions add and subtract implemented.
    - Implement sum, multiply, power and factorial functions.

--- foundations/solution_12_calculator/calculator.js
+++ foundations/solution_12_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+// @ts-check
+
+/** @param {number} num1 
+ * @param {number} num2 
+*/
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+/** @param {number} num1 
+ * @param {number} num2 
+*/
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+/** @param {number[]} values */
+const sum = function(values) {
+	return values.reduce((accumulator, value) => accumulator + value, 0);
 };
 
-const multiply = function() {
+/** @param {number[]} values */
+const multiply = function(values) {
+  return values.reduce((accumulator, value) => accumulator * value);
+}
 
+/** @param {number} base 
+ * @param {number} power
+*/
+const power = function(base, power) {
+  return Math.pow(base, power);
 };
 
-const power = function() {
-	
-};
+/** @param {number} value */
+const factorial = function(value) {
 
-const factorial = function() {
-	
+  let output = 1;
+	for (let i = value; i > 0; i--) {
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line
