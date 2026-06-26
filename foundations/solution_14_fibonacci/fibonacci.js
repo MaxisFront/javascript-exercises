28f880e79beb9677db8c99908bca01d713d331f2
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-25 19:02:42 -0600

    feat(fibonacci): complete all tests
    Return the specified position

--- foundations/solution_14_fibonacci/fibonacci.js
+++ foundations/solution_14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+// @ts-check
 
+/** @param {number} index */
+const fibonacci = function(index) {
+
+  const indexValue = Number(index);
+
+  if (indexValue < 0) return "OOPS";
+  if (indexValue === 0) return 0;
+  if (indexValue === 1) return 1;
+
+  let fiboActual = 1;
+  let fiboPrevious = 1;
+
+  for (let i = 2; i <= indexValue; i++) {
+    let sum = fiboActual + fiboPrevious;
+    fiboPrevious = fiboActual;
+    fiboActual = sum;
+
+  }
+
+  return fiboActual;
 };
 
 // Do not edit below this line
