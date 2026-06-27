2d4db2a2220efd988df75c90cdb4f503ebf5c58b
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-27 15:45:09 -0600

    feat(findTheOldest): complete all tests
    Return the object person with the oldest age through the use of reduce()

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
+// @ts-check
 
-};
+/** @param {{name: string, yearOfBirth: number, yearOfDeath?: number}[]} people */
+const findTheOldest = function(people) {
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
--- foundations/solution_16_findTheOldest/findTheOldest.js
+++ foundations/solution_16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+// @ts-check
 
-};
+/** @param {{name: string, yearOfBirth: number, yearOfDeath?: number}[]} people */
+const findTheOldest = function(people) {
+
+  return people.reduce((oldest, person) => {
+    return getYearsLived(person) > getYearsLived(oldest) ? person : oldest;
+  });
+}
+
+/** @param {{name: string, yearOfBirth: number, yearOfDeath?: number}} person*/
+function getYearsLived(person) {
+  const deathYear = person.yearOfDeath || new Date().getFullYear();
+  return deathYear - person.yearOfBirth;
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
